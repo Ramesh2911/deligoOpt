66c9e4972c2ff46e24bbaa666d0519c8a7e9b798
Ramesh Jana <81910860+Ramesh2911@users.noreply.github.com>
2025-07-25 13:19:14 +0530

    Add files via upload

---
+++ routes/otpRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import {
+   sendOTP,
+   verifyOTP,
+   resendOTP,
+}
+   from '../controllers/otpController.js';
+
+const otpRoutes = express.Router();
+otpRoutes.post('/userphoneverifiy', sendOTP);
+otpRoutes.post('/otpvarification', verifyOTP);
+otpRoutes.post('/resendotp', resendOTP);
+
+export default otpRoutes;
